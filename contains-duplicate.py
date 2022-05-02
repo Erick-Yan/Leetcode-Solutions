f559ae37f7edfd6e8356251b9b5ca032981000ce
Erick Yan <erick.yanzhang@gmail.com>
2022-05-02 12:51:42 -0400

    Completed Contains Duplicate

---
+++ contains-duplicate.py
@@ -0,0 +1,14 @@
+'''
+    My Solution: Create a list to track list members that've already appeared. Return false if a number already exists in that list, else continue iterating. 
+    Return true if the list has been iterated through.
+
+    Better Solution: Check if the list length is equal to the list length of its set.
+'''
+class Solution(object):
+    def containsDuplicate(self, nums):
+        appeared = []
+        for i in nums:
+            if i in appeared:
+                return True
+            appeared.append(i)
+        return False
